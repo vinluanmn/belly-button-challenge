d18fa60bd2f74999f916c625e2c8dfd22ad3ff00
vinluanmn <144140089+vinluanmn@users.noreply.github.com>
2024-01-10 00:36:13 -0500

    commit barChart and bubble
    completed charts

--- Starter_Code/StarterCode/static/js/app.js
+++ Starter_Code/StarterCode/static/js/app.js
@@ -21,7 +21,7 @@ function demoInfo(sample)
 
         // access index 0 from the array
         let resultData = result[0];
-        console.log(resultData);
+        //console.log(resultData);
 
         // clear the metadata out
         d3.select("#sample-metadata").html(""); 
@@ -35,7 +35,109 @@ function demoInfo(sample)
     });
 }
 
-// function that builds the graphs
+// function that builds the bar chart
+function buildBarChart(sample)
+{
+    // console.log(sample);
+    //let data = d3.json("samples.json");
+    //console.log(data);
+
+    d3.json("samples.json").then((data) => {
+        // grab all of the sample data
+        let sampleData = data.samples;
+        //console.log(sampleData);
+
+        
+        // filter based on the value of the sample
+        // (should return 1 result in an array based on the dataset)
+        let result = sampleData.filter(sampleResult => sampleResult.id == sample);
+        // console.log(result);
+        
+        // access index 0 from the array
+        let resultData = result[0];
+        console.log(resultData);
+
+        // get the otu_id, and sample_balues
+        let otu_ids = resultData.otu_ids;
+        let otu_labels = resultData.otu_labels;
+        let sample_values = resultData.sample_values;
+        console.log(otu_ids);
+        console.log(otu_labels);
+        console.log(sample_values);
+
+        // build the bar chart
+        // get the yticks
+        let yticks = otu_ids.slice(0, 10).map(id => `OTU ${id}`);
+        let xvalues = sample_values.slice(0, 10);
+        let textLabels = otu_labels.slice(0, 10);
+        console.log(textLabels);
+
+        let barChart = {
+            y: yticks.reverse(), 
+            x: xvalues.reverse(),
+            text: textLabels.reverse(),
+            type: "bar",
+            orientation: "h"
+        }
+
+        let layout = {
+            title: "Top 10 Belly Button Bacteria"
+        };
+
+        Plotly.newPlot("bar", [barChart], layout);
+    });
+}
+
+// function that builds the bubble chart
+function buildBubbleChart(sample)
+{
+    d3.json("samples.json").then((data) => {
+        // grab all of the sample data
+        let sampleData = data.samples;
+        //console.log(sampleData);
+
+        
+        // filter based on the value of the sample
+        // (should return 1 result in an array based on the dataset)
+        let result = sampleData.filter(sampleResult => sampleResult.id == sample);
+        // console.log(result);
+        
+        // access index 0 from the array
+        let resultData = result[0];
+        console.log(resultData);
+
+        // get the otu_id, and sample_balues
+        let otu_ids = resultData.otu_ids;
+        let otu_labels = resultData.otu_labels;
+        let sample_values = resultData.sample_values;
+        console.log(otu_ids);
+        console.log(otu_labels);
+        console.log(sample_values);
+
+        // build the bubble chart
+
+        let bubbleChart = {
+            y: sample_values, 
+            x: otu_ids,
+            text: otu_labels,
+            mode: "markers",
+            marker: {
+                size: sample_values, 
+                color: otu_ids,
+                colorscale: "Earth"
+            }
+        }
+
+        let layout = {
+            title: "Bacteria Cultures per Sample",
+            hovermode: "closest",
+            xaxis: {title: "OTU ID"}
+        };
+
+        Plotly.newPlot("bubble", [bubbleChart], layout);
+
+    });
+}
 
 // function that initializes the dashboard
 function initialize()
@@ -63,6 +165,10 @@ function initialize()
 
         // call the function to build the metadata
         demoInfo(sample01);
+        // call function to build the bar chart
+        buildBarChart(sample01);
+        // call function to build the bubble chart
+        buildBubbleChart(sample01)
     });
 
 }
@@ -72,6 +178,10 @@ function optionChanged(item)
 {
     // call the update to the metadata
     demoInfo(item);
+    // call function to build the bar chart
+    buildBarChart(item);
+    // call function to build the bubble chart
+    buildBubbleChart(item);
 }
 // call the initialized function
 initialize();
